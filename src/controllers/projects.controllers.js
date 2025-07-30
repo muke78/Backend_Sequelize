c690a6345e9b822960f1e347f7f6c77a9d7d3ef1
Erick Muke <muke7881@gmail.com>
2025-07-30 01:55:30 -0600

    feat(crud): Se completa el crud de proyectos

--- src/controllers/projects.controllers.js
+++ src/controllers/projects.controllers.js
@@ -12,6 +12,25 @@ export const getProjects = async (req, res) => {
   }
 };
 
+export const getProject = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const project = await Project.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!project)
+      return res.status(404).json({ message: 'Project does not exist' });
+    res.json(project);
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
+
 export const createProject = async (req, res) => {
   const { name, priority, description } = req.body;
 
@@ -23,6 +42,47 @@ export const createProject = async (req, res) => {
     });
 
     res.json(newProject);
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
+
+export const updateProject = async (req, res) => {
+  const { id } = req.params;
+  const { name, priority, description } = req.body;
+
+  try {
+    const project = await Project.findByPk(id);
+    project.name = name;
+    project.priority = priority;
+    project.description = description;
+    await project.save();
+
+    res.json(project);
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
+
+export const deleteProject = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { name } = await Project.findByPk(id);
+    await Project.destroy({
+      where: {
+        id,
+      },
+    });
+    res.status(200).json({
+      success: true,
+      message: `Se borro con exito el proyecto ${name}`,
+    });
   } catch (error) {
     return res.status(500).json({
       status: false,
--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -2,14 +2,17 @@ import { Router } from 'express';
 import {
   createProject,
   getProjects,
+  deleteProject,
+  updateProject,
+  getProject,
 } from '../controllers/projects.controllers.js';
 
 const router = Router();
 
 router.get('/projects', getProjects);
 router.post('/projects', createProject);
-// router.get('/projects/:id');
-// router.put('/projects/:id');
-// router.delete('/projects/:id');
+router.get('/projects/:id', getProject);
+router.put('/projects/:id', updateProject);
+router.delete('/projects/:id', deleteProject);
 
 export default router;
